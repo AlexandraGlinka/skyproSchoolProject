d653512d94af1f81f30bf8d087219b942a877bbe
glinka <alexandraglinka@yandex.ru>
2023-08-09 12:56:10 +0300

    service impl

---
+++ src/main/java/ru/hogwarts/service/StudentServiceImpl.java
@@ -0,0 +1,53 @@
+package ru.hogwarts.service;
+
+import ru.hogwarts.exceptions.FacultyNotFoundException;
+import ru.hogwarts.exceptions.StudentNotFoundException;
+import ru.hogwarts.model.Faculty;
+import ru.hogwarts.model.Student;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class StudentServiceImpl implements StudentService {
+    Map<Long, Student> students = new HashMap<>();
+    @Override
+    public Student addStudent(Student student) {
+        students.put(student.getId(), student);
+        return student;
+    }
+
+    @Override
+    public Student getStudentById(Long id) {
+        if (!students.containsKey(id)) {
+            throw new StudentNotFoundException("Student not found");
+        }
+        return students.get(id);
+    }
+
+    @Override
+    public Collection<Student> getAllStudents() {
+        return Collections.unmodifiableCollection(students.values());
+    }
+
+    @Override
+    public Student updateStudent(Long id, Student student) {
+        if (!students.containsKey(id)) {
+            throw new StudentNotFoundException("Student not found");
+        }
+        Student studentUpdate = students.get(id);
+        studentUpdate.setName(student.getName());
+        studentUpdate.setAge(student.getAge());
+        students.put(id, student);
+        return studentUpdate;
+    }
+
+    @Override
+    public void deleteStudent(Long id) {
+        if (!students.containsKey(id)) {
+            throw new StudentNotFoundException("Student not found");
+        }
+        students.remove(id);
+    }
+}
